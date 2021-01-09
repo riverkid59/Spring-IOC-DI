d01d53c64f5a3999bd52c6c562ed21c33ac14307
Josselin EVRARD <josselin.evrard@sncf.fr>
2021-01-09 22:28:41 +0100

    terminé

--- src/main/java/com/wildcodeschool/wildandwizard/repository/WizardRepository.java
+++ src/main/java/com/wildcodeschool/wildandwizard/repository/WizardRepository.java
@@ -7,8 +7,13 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class WizardRepository implements WizardDao {
+import org.springframework.stereotype.Repository;
 
+@Repository
+public class WizardRepository implements WizardDao {
+	
+	
+	@SuppressWarnings("deprecation")
     private static List<Wizard> wizards = new ArrayList<>(
             Arrays.asList(
                     new Wizard(1L, "Harry", "Potter", new Date(80, 6, 31), "London", "", false)
